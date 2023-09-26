d81f67a60c025558be9ebce6c421727fc30e6dee
Danny Heeley <danny.heeley@bjss.com>
2023-09-26 14:50:02 +0100

    in a good place but cannot drag to drawn walls/weighted nodes

--- PathfinderVisualiser/src/PathfinderVisualiser/Node/Node.jsx
+++ PathfinderVisualiser/src/PathfinderVisualiser/Node/Node.jsx
@@ -1,60 +1,66 @@
-import React from "react";
+import React, { memo } from "react";
 import "./Node.css";
 
-export function initialiseNode(
-  col,
-  row,
-  startNodeCol,
-  startNodeRow,
-  targetNodeCol,
-  targetNodeRow
-) {
-  return {
+export const useNode = () => {
+  const initialiseNode = (
     col,
     row,
-    isStart: row === startNodeRow && col === startNodeCol,
-    isTarget: row === targetNodeRow && col === targetNodeCol,
-    distance: Infinity,
-    isVisited: false,
-    isWall: false,
-    isWeighted: false,
-    previousNode: null,
-    gScore: row == startNodeRow && col === startNodeCol ? 0 : Infinity,
-    fScore: Infinity,
-    cameFrom: null,
+    startNodeCol,
+    startNodeRow,
+    targetNodeCol,
+    targetNodeRow
+  ) => {
+    return {
+      col,
+      row,
+      isStart: row === startNodeRow && col === startNodeCol,
+      isTarget: row === targetNodeRow && col === targetNodeCol,
+      distance: Infinity,
+      isVisited: false,
+      isWall: false,
+      isWeighted: false,
+      previousNode: null,
+      gScore: row == startNodeRow && col === startNodeCol ? 0 : Infinity,
+      fScore: Infinity,
+      cameFrom: null,
+    };
   };
-}
 
-export function Node({
-  col,
-  row,
-  isTarget,
-  isStart,
-  isWall,
-  isWeighted,
-  isVisited,
-  onMouseDown,
-  onMouseEnter,
-  onMouseUp,
-}) {
-  const extraClassName = isTarget
-    ? "node-target"
-    : isStart
-    ? "node-start"
-    : isWall
-    ? "node-wall"
-    : isVisited
-    ? "node-visited"
-    : isWeighted
-    ? "node-weighted"
-    : "node";
-  return (
-    <div
-      id={`node-${row}-${col}`}
-      className={`node ${extraClassName}`}
-      onMouseDown={() => onMouseDown(row, col)}
-      onMouseEnter={() => onMouseEnter(row, col)}
-      onMouseUp={() => onMouseUp()}
-    ></div>
-  );
-}
+  const Node = ({
+    col,
+    row,
+    isTarget,
+    isStart,
+    isWall,
+    isWeighted,
+    isVisited,
+    onMouseDown,
+    onMouseEnter,
+    onMouseUp,
+  }) => {
+    const extraClassName = isTarget
+      ? "node-target"
+      : isStart
+      ? "node-start"
+      : isWall
+      ? "node-wall"
+      : isVisited
+      ? "node-visited"
+      : isWeighted
+      ? "node-weighted"
+      : "node";
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}
+      ></div>
+    );
+  };
+  return {
+    initialiseNode,
+    Node,
+  };
+};
